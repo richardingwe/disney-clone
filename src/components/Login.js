3e12caa69003f86e4670c94b2a0ecc107d99843a
richardingwe <richardingwe5@gmail.com>
2021-04-30 11:20:26 +0100

    added basic styles to login

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,11 +2,34 @@ import styled from 'styled-components';
 
 const Login = (props) => {
     return (
-        <div>
-            <h1>Login</h1>
-            <h1>hello from disney</h1>
-        </div>
+        <Container>
+            <Content>
+                Content
+            </Content>
+        </Container>
     );
 };
+
+const Container = styled.section`
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    height: 100vh;
+`;
+
+const Content = styled.div`
+    margin-bottom: 10vh;
+    width: 100%;
+    position: relative;
+    min-height: 100vh;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 80px 40px;
+    height: 100%;
+`;
 
 export default Login;
