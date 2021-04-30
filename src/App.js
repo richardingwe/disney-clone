d5a12e00b331cc1271effbcade6cea0e4af26c56
richardingwe <richardingwe5@gmail.com>
2021-04-30 20:49:22 +0100

    added disney logo to header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Login from './components/Login.js';
-import Header from './components/Header.js';
+import Login from './components/Login';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
