92cd8b5eb173d4c7256ce3bd3e76b696fa8d52e4
richardingwe <richardingwe5@gmail.com>
2021-04-30 10:35:32 +0100

    created the login component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/Login.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello from disney</h1>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Login = (props) => {
+    return (
+        <div>
+            <h1>Login</h1>
+            <h1>hello from disney</h1>
+        </div>
+    );
+};
+
+export default Login;
