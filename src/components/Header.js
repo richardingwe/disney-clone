d87bd614868afd540aceccce36cb6fc217b12d9b
richardingwe <richardingwe5@gmail.com>
2021-05-01 01:35:26 +0100

    worked on navmenu

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,32 @@ const Header = (props) => {
             <Logo>
                 <img src="/images/logo.svg" alt="disney logo" />
             </Logo>
+            <NavMenu>
+                <a href="/home">
+                    <img src="/images/home-icon.svg" alt="home-icon" />
+                    <span>HOME</span>
+                </a>
+                <a href="/home">
+                    <img src="/images/search-icon.svg" alt="search-icon" />
+                    <span>SEARCH</span>
+                </a>
+                <a href="/home">
+                    <img src="/images/watchlist-icon.svg" alt="watchlist-icon" />
+                    <span>WATCHLIST</span>
+                </a>
+                <a href="/home">
+                    <img src="/images/original-icon.svg" alt="original-icon" />
+                    <span>ORIGINALS</span>
+                </a>
+                <a href="/home">
+                    <img src="/images/movie-icon.svg" alt="movies-icon" />
+                    <span>MOVIES</span>
+                </a>
+                <a href="/home">
+                    <img src="/images/series-icon.svg" alt="series-icon" />
+                    <span>SERIES</span>
+                </a>
+            </NavMenu>
         </Nav>
     );
 };
@@ -35,6 +61,68 @@ const Logo = styled.div`
     img {
         display: block;
         width: 100%;
+    }
+`;
+
+const NavMenu = styled.div`
+    align-items: center;
+    display: flex;
+    flex-flow: row nowrap;
+    height: 100%;
+    justify-content: flex-end;
+    margin: 0;
+    padding: 0;
+    position: relative;
+    margin-right: auto;
+    margin-left: 25px;
+
+    a {
+        display: flex;
+        align-items: center;
+        padding: 0 12px;
+
+        img {
+            height: 20px;
+            min-width: 20px;
+            width: 20px;
+            z-index: auto;
+        }
+
+        span {
+            color: rgb(249, 249, 249);
+            font-size: 13px;
+            letter-spacing: 1.42px;
+            line-height: 1.08;
+            padding: 2px 0;
+            white-space: nowrap;
+            position: relative;
+
+            &::before {
+                content: '';
+                background-color: rgb(249, 249, 249);
+                border-radius: 0px 0px 4px 4px;
+                bottom: -6px;
+                left: 0px;
+                height: 2px;
+                opacity: 0;
+                position: absolute;
+                right: 0px;
+                transform-origin: left center;
+                transform: scaleX(0);
+                transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+                visibility: hidden;
+                width: auto;
+            }
+        }
+        &:hover span::before {
+            transform: scaleX(1);
+            visibility: visible;
+            opacity: 1 !important;
+        }
+    }
+
+    @media (max-width: 768px) {
+        display: none;
     }
 `;
 
