d20af3b87dc3a6f328592e2e1439eb1038005d62
richardingwe <richardingwe5@gmail.com>
2021-05-01 01:42:30 +0100

    worked on the login button

--- src/components/Header.js
+++ src/components/Header.js
@@ -32,6 +32,7 @@ const Header = (props) => {
                     <span>SERIES</span>
                 </a>
             </NavMenu>
+            <Login>Login</Login>
         </Nav>
     );
 };
@@ -123,6 +124,22 @@ const NavMenu = styled.div`
 
     @media (max-width: 768px) {
         display: none;
+    }
+`;
+
+const Login = styled.a`
+    background-color: rgba(0,0,0,0.6);
+    padding: 8px 16px;
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+    border: 1px solid #f9f9f9;
+    border-radius: 4px;
+    transition: all 200ms ease 0s;
+
+    &:hover {
+        background-color: #f9f9f9;
+        color: #000;
+        border-color: transparent;
     }
 `;
 
