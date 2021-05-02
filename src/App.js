cb85ce79895b240af9cb7a4d1920011eccea65c5
richardingwe <richardingwe5@gmail.com>
2021-05-02 22:05:48 +0100

    created the detail page.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Login from './components/Login';
 import Header from './components/Header';
 import Home from './components/Home';
 import './App.css';
+import Detail from './components/Detail';
 
 function App() {
   return (
@@ -15,6 +16,9 @@ function App() {
           </Route>
           <Route exact path='/home'>
             <Home />
+          </Route>
+          <Route exact path='/detail/:id'>
+            <Detail />
           </Route>
         </Switch>
       </Router>
---
+++ src/components/Detail.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Detail = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Detail;
