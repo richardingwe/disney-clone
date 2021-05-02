bdfaefa0947de88e1901c4aaeb2171e0ffc16990
richardingwe <richardingwe5@gmail.com>
2021-05-02 10:29:33 +0100

    finished the viewers section

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -74,6 +74,25 @@ const Wrap = styled.div`
         z-index: 1;
         top: 0;
     }
+
+    video {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0px;
+        opacity: 0;
+        z-index: 0;
+    }
+
+    &:hover {
+        box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px, rgba(0 0 0 / 73%) 0px 16px 10px -10px;
+        transform: scale(1.05); 
+        border-color: rgba(249, 249, 249, 0.8);
+        
+        video {
+            opacity: 1;
+        }
+    }
 `;
 
 export default Viewers;
